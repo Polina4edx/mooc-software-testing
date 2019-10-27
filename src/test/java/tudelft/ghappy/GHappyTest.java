9eed02d9c782eea270955c54e82754ccc3dcf677
juan Gomez <jcgom2003@yahoo.com>
2019-10-26 22:39:55 -0400

    Complete the test class for gHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,28 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void doubleg () {
+        GHappy gh = new GHappy ();
+        boolean result = gh.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+
+
+    }
+
+    @Test
+    public void singleg () {
+        GHappy gh = new GHappy ();
+        Assertions.assertFalse(gh.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void doubleandsingleg () {
+        GHappy gh = new GHappy ();
+        Assertions.assertFalse(gh.gHappy("xxggyygxx"));
+    }
 }
